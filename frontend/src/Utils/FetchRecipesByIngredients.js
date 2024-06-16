7c28cd6642ab57d3cc67c9ff25a7fee2ed4321d7
Renan Parise <renan.parise@acessoenergia.com.br>
2024-06-15 23:23:59 -0300

    chore: Refactor FetchRecipesByIngredients.js to improve performance and caching

--- frontend/src/Utils/FetchRecipesByIngredients.js
+++ frontend/src/Utils/FetchRecipesByIngredients.js
@@ -1,36 +1,38 @@
 const fetchRecipesByIngredients = async (user, setLoadingRecipes, setRecipes) => {
     setLoadingRecipes(true);
     const userIngredients = user ? user.ingredients.join(',') : '';
-    const localStorageKey = `recipes_${userIngredients}`;
-    const maxStoredRequests = 5;
+    const localStorageKey = 'storedRecipes';
 
-    let storedRequests = JSON.parse(localStorage.getItem('storedRequests')) || [];
-    const requestIndex = storedRequests.indexOf(localStorageKey);
+    let storedRecipes = JSON.parse(localStorage.getItem(localStorageKey)) || {};
 
-    if (requestIndex === -1 && storedRequests.length >= maxStoredRequests) {
-        const firstStoredRequest = storedRequests.shift();
-        localStorage.removeItem(firstStoredRequest);
-    }
-
-    storedRequests = storedRequests.filter(req => req !== localStorageKey);
-    storedRequests.push(localStorageKey);
-    localStorage.setItem('storedRequests', JSON.stringify(storedRequests));
-
-    const storedData = localStorage.getItem(localStorageKey);
-    if (storedData) {
-        setRecipes(JSON.parse(storedData));
+    if (storedRecipes[userIngredients]) {
+        setRecipes(storedRecipes[userIngredients]);
         setLoadingRecipes(false);
         return;
     }
 
+    const maxStoredRequests = 15;
+    let storedRequests = JSON.parse(localStorage.getItem('storedRequests')) || [];
+    const requestIndex = storedRequests.indexOf(userIngredients);
+
+    if (requestIndex === -1 && storedRequests.length >= maxStoredRequests) {
+        const firstStoredRequest = storedRequests.shift();
+        delete storedRecipes[firstStoredRequest];
+    }
+
+    storedRequests = storedRequests.filter(req => req !== userIngredients);
+    storedRequests.push(userIngredients);
+    localStorage.setItem('storedRequests', JSON.stringify(storedRequests));
+
     try {
         const response = await fetch(`/api/v1/recipes/by-multiple-criteria?ingredient=${userIngredients}`);
         const data = await response.json();
+        
         if (Array.isArray(data)) {
             const filteredRecipes = data.filter(recipe => recipe.percentage >= 100);
+            storedRecipes[userIngredients] = filteredRecipes;
+            localStorage.setItem(localStorageKey, JSON.stringify(storedRecipes));
             setRecipes(filteredRecipes);
-
-            localStorage.setItem(localStorageKey, JSON.stringify(filteredRecipes));
         } else {
             console.error('Error: Data is not an array');
         }
